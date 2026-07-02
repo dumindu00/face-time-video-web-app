6daa559a937950f3d4f0491d72bbee505603565b
Your Name <you@example.com>
2026-07-02 20:44:30 +0530

    settinup Socket.IO & directing user to videoChat page

--- server/server.js
+++ server/server.js
@@ -2,8 +2,22 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const userRoutes = require("./routes/userRoutes");
+const http = require("http")
+const { Server } = require("socket.io");
 
 const app = express()
+
+const server = http.createServer(app)
+
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+})
+
+
 
 app.use(cors())
 app.use(express.json())
@@ -14,8 +28,17 @@ app.get("/", (req, res) => {
     res.send("Face-Time Server Running")
 })
 
+
+io.on("connection", (socket) => {
+        console.log("New User Connected")
+
+        socket.on("disconnected", () => {
+            console.log("User Disconnected")
+        })
+    })
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log("Server Started")
+server.listen(5000, () => {
+    console.log(`Server running on ${PORT}`);
 })
